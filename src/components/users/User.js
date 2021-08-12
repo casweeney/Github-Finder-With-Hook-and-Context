723ea5b8ac87b234d5de778648f0174952025231
Casweeney Ojukwu <casweeno2000@gmail.com>
2021-08-12 17:35:46 +0100

    Completed

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,17 +1,20 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useContext } from 'react';
 import Spinner from '../layout/Spinner';
 import Repos from '../repos/Repos';
-import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import GithubContext from '../../context/github/githubContext';
 
 
-const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
+const User = ({ match }) => {
+    const githubContext = useContext(GithubContext);
+
+    const {getUser, loading, user, repos, getUserRepos} = githubContext;
+
     useEffect(() => {
         getUser(match.params.login);
         getUserRepos(match.params.login);
         // eslint-disable-next-line
     }, []);
-
     
     const { 
         name,
@@ -80,7 +83,7 @@ const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
             <div className="card text-center">
                 <div className="badge badge-primary">Followers: {followers}</div>
                 <div className="badge badge-success">Following: {following}</div>
-                <div className="badge badge-light">Public Repos: {public_repos}</div>
+                <div className="badge badge-danger">Public Repos: {public_repos}</div>
                 <div className="badge badge-dark">Public Gists: {public_gists}</div>
             </div>
 
@@ -88,14 +91,6 @@ const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
         </Fragment>
     );
     
-}
-
-User.propTypes = {
-    loading: PropTypes.bool,
-    user: PropTypes.object.isRequired,
-    repos: PropTypes.array.isRequired,
-    getUser: PropTypes.func.isRequired,
-    getUserRepos: PropTypes.func.isRequired
 }
 
 export default User
